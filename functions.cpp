7dec52a24d5aac880efac219e7a25bfcfb3f5ea2
Teodor Kostadinov <tedi.sv.kostadinov@gmail.com>
2022-01-23 06:04:52 +0200

    Added Register and MainMenu

--- functions.cpp
+++ functions.cpp
@@ -185,7 +185,42 @@ bool String_Contains_Digit(std::string &str)
     return false;
 }
 
-bool LoginUsername(int userNumber)
+bool Password_Check(std::string &password)
+{
+    if (!is_Password_Characters_OK(password))
+    {
+        std::cout << "Password contains forbidden characters!\n";
+        return false;
+    }
+    if (!is_Password_size_OK(password))
+    {
+        std::cout << "Password must contain at least 5 symbols!\n";
+        return false;
+    }
+    if (!String_Contains_Digit(password))
+    {
+        std::cout << "Password must contain at least 1 digit!\n";
+        return false;
+    }
+    if (!String_Contains_UpperCase(password))
+    {
+        std::cout << "Password must contain at least 1 upper case letter!\n";
+        return false;
+    }
+    if (!String_Contains_LowerCase(password))
+    {
+        std::cout << "Password must contain at least 1 lower case letter!\n";
+        return false;
+    }
+    if (!String_Contains_Symbol(password))
+    {
+        std::cout << "Password must contain at least 1 special symbol!\n";
+        return false;
+    }
+    return true;
+}
+
+bool LoginUsername(int &userNumber)
 {
     std::cout << "Enter username: ";
     std::string username;
@@ -204,6 +239,7 @@ bool LoginUsername(int userNumber)
         {
             userNumber = i;
             isUsernameSaved = true;
+            break;
         }
     }
 
@@ -229,30 +265,124 @@ bool LoginPassword(const int userNumber)
     if (password != Users[userNumber].password)
     {
         std::cout << "Password is incorrect! Try again!\n";
+        return false;
     }
 
     return true;
 }
 
-void Login()
-{
+void Login(){
     int userNumber = -1;
     bool Loop_Username = true;
-    while (Loop_Username)
-    {
+    while(Loop_Username){
         Loop_Username = !LoginUsername(userNumber);
-    }
+    }   
     bool Loop_Password = true;
-    while (Loop_Password)
-    {
+    while(Loop_Password) {
         Loop_Password = !LoginPassword(userNumber);
+    }//add exist if password is not guessed
+}
+
+bool RegisterUsername(std::string &username)
+{
+    std::cout << "Enter a new username: ";
+    std::getline(std::cin, username);
+
+    if (!is_Username_OK(username))
+    {
+        std::cout << "Please enter a valid username!\n";
+        return false;
     }
+
+    for (int i = 0; i < Users.size(); i++)
+    {
+        if (username == Users[i].username)
+        {
+            std::cout << "Username is already used!\n";
+            return false;
+            break;
+        }
+    }
+
+    return true;
+
+}
+
+bool RegisterPassword(std::string &password)
+{
+    std::cout << "Enter password: ";    
+    std::getline(std::cin, password);
+
+    if(!Password_Check(password)){
+        return false;
+    }
+
+    return true;
+}
+
+bool RegisterPassword_Repeat(std::string &password)
+{
+    std::string rep_password;
+    std::cout << "Repeat password: ";    
+    std::getline(std::cin, rep_password);
+
+    if(rep_password != password)
+    {
+        std::cout << "Incorrect password confirmation!\n";
+        return false;
+    }
+
+    return true;
+}
+
+void Register(){
+
+    std::string username;
+    std::string password;
+
+    bool Loop_Username = true;
+    while(Loop_Username){
+        Loop_Username = !RegisterUsername(username);
+        username = "";
+    } 
+
+    bool Loop_Password = true;
+    while(Loop_Password) {
+        Loop_Password = !RegisterPassword(password);
+        if(!Loop_Password){
+           Loop_Password = !RegisterPassword_Repeat(password);
+        }
+    }
+}
+
+void MainMenuLogic()
+{  
+    std::string HomeMenuCommand;
+    bool endProgram = false;
+    do
+    {
+        std::cout << "Choose a command: "; 
+        std::getline(std::cin, HomeMenuCommand);
+        if (HomeMenuCommand == "L")
+        {
+            Login();
+        }
+        if (HomeMenuCommand == "R")
+        {
+            Register();
+        }
+        if (HomeMenuCommand == "Q")
+        {
+            endProgram = true;
+        }
+    } while (!endProgram);
 }
 
 int main()
 {
     LoadProfiles(Users);
-    Login();
+    MainMenuLogic();
+
 
     return 0;
 }
